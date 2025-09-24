6f000dcbb81393328fdc64b062203df42e725fef
shahadathhs <shahadathhossensajib732@gmail.com>
2025-09-25 02:45:33 +0600

    feat: update env

--- src/common/jwt/jwt.decorator.ts
+++ src/common/jwt/jwt.decorator.ts
@@ -16,8 +16,8 @@ export const GetUser = createParamDecorator(
   (key: keyof JWTPayload | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<RequestWithUser>();
     const user = request.user;
-
-    return key ? user?.[key] : user;
+console.log("user",user);
+    return key ? user?.sub : user;
   },
 );
 
