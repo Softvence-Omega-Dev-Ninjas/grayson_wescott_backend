285431625f096351377abf02e2a407b513802781
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-06 03:09:52 +0600

    feat: progress stats endpoint setup completed

--- src/main/admin/progress/progress.controller.ts
+++ src/main/admin/progress/progress.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ValidateAdmin } from '@project/common/jwt/jwt.decorator';
 import { ProgressStatsService } from './services/progress-stats.service';
 
+@ApiTags('Admin --- Progress Tracking')
+@ApiBearerAuth()
+@ValidateAdmin()
 @Controller('progress')
 export class ProgressController {
   constructor(private readonly progressStatsService: ProgressStatsService) {}
+
+  @ApiOperation({ summary: 'Get progress stats' })
+  @Get('stats')
+  async getStats() {
+    return this.progressStatsService.getProgressStats();
+  }
 }
--- src/main/admin/progress/services/progress-stats.service.ts
+++ src/main/admin/progress/services/progress-stats.service.ts
@@ -1,7 +1,48 @@
 import { Injectable } from '@nestjs/common';
-
+import { HandleError } from '@project/common/error/handle-error.decorator';
+import {
+  successResponse,
+  TResponse,
+} from '@project/common/utils/response.util';
+import { PrismaService } from '@project/lib/prisma/prisma.service';
 
 @Injectable()
 export class ProgressStatsService {
-  
+  constructor(private readonly prisma: PrismaService) {}
+
+  @HandleError('Failed to fetch progress stats')
+  async getProgressStats(): Promise<TResponse<any>> {
+    const outPutData = {
+      clients: {
+        activeClients: 40,
+        addedThisMonth: 10,
+        addedThisWeek: 5,
+      },
+      program: {
+        totalPrograms: 15,
+        activePrograms: 10,
+        programsAddedThisMonth: 3,
+        programsAddedThisWeek: 1,
+      },
+      completion: {
+        avgProgramCompletionRate: '65%',
+        completionRateIncrementThisWeek: '5%',
+        completionRateIncrementThisMonth: '8%',
+      },
+      adherenceCompletion: {
+        overallAdherenceRate: '70%',
+        adherenceRateThisWeek: '75%',
+        adherenceRateThisMonth: '72%',
+      },
+      workout: {
+        totalWorkoutsLogged: 500,
+        completedWorkouts: 350,
+        completedToday: 20,
+        completedThisWeek: 100,
+        completedThisMonth: 200,
+      },
+    };
+
+    return successResponse(outPutData, 'Progress stats fetched successfully');
+  }
 }
