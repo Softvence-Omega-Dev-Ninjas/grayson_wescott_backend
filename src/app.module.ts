dabffa3fa770a54dd7ab44683138b9bb3c6ddd0e
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-12 00:11:07 +0600

    fix: fixed docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,21 +35,11 @@ import { MainModule } from './main/main.module';
       useFactory: async (configService: ConfigService) => {
         const host = configService.getOrThrow<string>(ENVEnum.REDIS_HOST);
         const port = configService.getOrThrow<string>(ENVEnum.REDIS_PORT);
-        // const username = configService.get<string>(ENVEnum.REDIS_USERNAME);
-        // const password = configService.get<string>(ENVEnum.REDIS_PASSWORD);
 
         return {
           connection: {
             host,
             port: parseInt(port, 10),
-            // ...(username &&
-            //   password && {
-            //     username,
-            //     password,
-            //     tls: {
-            //       rejectUnauthorized: false,
-            //     },
-            //   }),
           },
         };
       },
