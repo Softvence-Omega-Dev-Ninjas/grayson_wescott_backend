6da3e84275926ca14f5b12363cc17f408e5e86c4
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-13 00:31:58 +0600

    feat: update assinged program dto

--- src/main/user/program/dto/currently-assigned-program.dto.ts
+++ src/main/user/program/dto/currently-assigned-program.dto.ts
@@ -1,6 +1,7 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { ProgramProgress } from '@prisma/client';
 import { PaginationDto } from '@project/common/dto/pagination.dto';
+import { Transform } from 'class-transformer';
 import { IsEnum, IsOptional } from 'class-validator';
 
 export class CurrentlyAssignedProgramDto extends PaginationDto {
@@ -9,6 +10,15 @@ export class CurrentlyAssignedProgramDto extends PaginationDto {
     enum: ProgramProgress,
   })
   @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      if (value === 'IN_PROGRESS') return ProgramProgress.IN_PROGRESS;
+      if (value === 'PAUSED') return ProgramProgress.PAUSED;
+      if (value === 'COMPLETED') return ProgramProgress.COMPLETED;
+      return undefined;
+    }
+    return undefined;
+  })
   @IsEnum(ProgramProgress)
   status?: ProgramProgress;
 }
